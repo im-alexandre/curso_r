af1fd7c603a57fc04a962450ef7540553cf89cd9
Alexandre Castro <im.alexandre07@gmail.com>
2024-07-11 14:57:34 -0300

    -

---
+++ primeiro_app_shiny.R
@@ -0,0 +1,29 @@
+library('shiny')
+library(bslib)
+
+ui <- page_sidebar(
+  tittle = "Meu app!",
+  sidebar = sidebar(
+    numericInput(
+      inputId = 'bins',
+      label = 'Number of bins:',
+      min = 1,
+      max = 10,
+      value = 40
+    )
+  ),
+  plotOutput(outputId = 'distPlot')
+)
+
+server <- function(input, output) {
+  output$distPlot = renderPlot({
+    x <- faithful$waiting
+    bins = seq(min(x), max(x), length.out = input$bins + 1)
+    print(bins)
+    hist(x, breaks = bins, col = '#007700', border = 'red',
+         xlab = 'Esperando tempo para a próxima explosão',
+         main = 'Histograma de tempos de espera')
+  })
+}
+
+shinyApp(ui = ui, server = server)
